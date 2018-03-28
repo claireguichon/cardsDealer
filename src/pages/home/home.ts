5e29116ed43e7775e66e0ef040b2657426c4e4e7
Josquin Cornec <josquin@victoria.frmug.org>
2018-03-28 11:40:02 +0200

    first commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,51 @@
 import { Component } from '@angular/core';
+import {Validators, FormBuilder, FormGroup } from '@angular/forms';
+import {HttpClient} from '@angular/common/http';
 import { NavController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  connexion:FormGroup;
 
+  constructor(public navCtrl: NavController, public formBuilder: FormBuilder, private http : HttpClient) {
+    this.connexion = this.formBuilder.group({
+      pseudo: ['', Validators.compose([Validators.minLength(3), Validators.required])],
+    });
   }
+  logForm(){
+      const url = "http://localhost:1341/cardsdealer/linq/";
 
+    var data = {
+      pseudo : this.connexion.value,
+      ready : false
+    };
+
+    const req = this.http.post(url, data)
+      .subscribe(
+        res => {
+          console.log("res"+res);
+        },
+        err => {
+          console.log("err"+err);
+        }
+      );
+
+    console.log("req"+req);
+
+    const req2 = this.http.get(url)
+      .subscribe(
+        res => {
+          console.log("res"+res);
+        },
+        err => {
+          console.log("err"+err);
+        }
+      );
+
+    console.log("req2"+req2);
+  }
 }
